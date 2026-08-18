7e204ec97bd76acccece5aaf0722689c36e21e01
rohailgulbaz <rohailgulbaz95@gmail.com>
2026-08-18 16:16:23 +0200

    Update server_usage.sh

--- monitoring/server_usage.sh
+++ monitoring/server_usage.sh
@@ -1,3 +1,3 @@
 systemd-inhibit --what=sleep --why="Server Usage Monitoring" \
 ssh <REMOTE_USER>@<REMOTE_HOST> 'bash -s' < monitor_usage.sh \
-| tee -a server_usage.csv
+| tee -a ~/data/server_usage.csv
