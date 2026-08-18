8f20a912e623f221aea31d33de6bc4b60e9cb1af
rohailgulbaz <rohailgulbaz95@gmail.com>
2026-08-18 15:49:24 +0200

    Update edge_power.sh

--- monitoring/edge_power.sh
+++ monitoring/edge_power.sh
@@ -1,10 +1,11 @@
-systemd-inhibit --what=sleep --why="Edge Power Monitoring" \
+systemd-inhibit --what=sleep --why="Power Monitoring" \
 ssh <REMOTE_USER>@<REMOTE_HOST> '
 echo "date,time,power_watts"
 
 mosquitto_sub -u <MQTT_USER> -P <MQTT_PASSWORD> \
-    -h <MQTT_BROKER> -p <MQTT_PORT> -t "<MQTT_TOPIC>" |
+    -h <MQTT_BROKER> -p <MQTT_PORT> \
+    -t "<MQTT_TOPIC>" |
 while read power; do
     echo "$(date +%Y-%m-%d),$(date +%H:%M:%S),$power"
 done
-' | tee -a edge_power_readings.csv
+' | tee -a ~/edge_power_readings.csv
