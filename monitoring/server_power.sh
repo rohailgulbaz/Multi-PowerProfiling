d447f00888d0235f8573323d0a83cf01bec18f6a
rohailgulbaz <rohailgulbaz95@gmail.com>
2026-08-18 15:20:06 +0200

    Update server_power.sh

--- monitoring/server_power.sh
+++ monitoring/server_power.sh
@@ -1,1 +1,12 @@
+systemd-inhibit --what=sleep --why="Server Power Monitoring" \
+ssh <REMOTE_USER>@<REMOTE_HOST> '
+echo "date,time,power_watts"
 
+while true; do
+    POWER=$(snmpget -v 1 -c <SNMP_COMMUNITY> -OvnQ \
+        <PDU_HOST>:<SNMP_PORT> <POWER_OID> 2>/dev/null)
+
+    echo "$(date +%Y-%m-%d),$(date +%H:%M:%S),$POWER"
+    sleep 1
+done
+' | tee -a server_power_readings.csv
